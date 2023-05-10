6456452290cae849cfb49c00903b3c5832141a95
krunalbhavsar31 <krunal93bhavsar@gmail.com>
2023-05-10 14:35:15 +0530

    Circle 1

--- week-1/day-2/exercise-1/Circle/Program.cs
+++ week-1/day-2/exercise-1/Circle/Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace Circle
 {
-    internal class Program
+    internal class AOC
     {
+        double Radius;
         static void Main(string[] args)
         {
-            // Create a Circle object and display its area and circumference
+            double radius = 0;
+            Console.WriteLine("Enter the value of radius:");
+            radius = Convert.ToDouble(Console.ReadLine());
+            AOC circle = new AOC(radius);
+            Console.WriteLine("Area Of Circle: " + circle.getarea());
+            Console.WriteLine("Aree of circumference: " + circle.circumference());
+        }
+        public AOC(double radius)
+        {
+            this.Radius = radius;
+        }
+        public double getarea()
+        {
+            double pi = 0;
+            double area = 0;
+            pi = 3.14;
+            area = (pi) * this.Radius * this.Radius;
+            return area;
+        }
+        public double circumference()
+        {
+            double circumference = 0;
+            double pi = 0;
+            pi = 3.14;
+            circumference = 2 * pi * this.Radius;
+            return circumference;
         }
     }
 }
