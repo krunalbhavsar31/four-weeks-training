252da20491018b47c439468e7ebf83ebafa1c1c8
krunalbhavsar31 <krunal93bhavsar@gmail.com>
2023-05-12 17:13:45 +0530

    CovariantAndContravariantGenerics 1

--- week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
+++ week-1/day-5/exercise-2/CovariantAndContravariantGenerics/Program.cs
@@ -10,7 +10,8 @@
         // Implement Process method
         public int Process(string input)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return input.Length;
         }
     }
 
@@ -19,7 +20,8 @@
         // Implement Process method
         public string Process(double input)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return input.ToString();
         }
     }
     internal class Program
@@ -27,6 +29,17 @@
         static void Main(string[] args)
         {
             // Demonstrate covariance and contravariance with IProcessor interface
+            // Demonstrating covariance
+            IProcessor<double, string> processor1 = new DoubleToStringProcessor();
+            string result1 = processor1.Process(3.14);
+            Console.WriteLine($"String representation of double: {result1}");
+
+            // Demonstrating contravariance
+            IProcessor<string, int> processor2 = new StringToIntProcessor();
+            IProcessor<string, int> processor3 = processor2;
+            int result2 = processor3.Process("Hello, world!");
+            Console.WriteLine($"String length: {result2}");
+
         }
     }
 }
