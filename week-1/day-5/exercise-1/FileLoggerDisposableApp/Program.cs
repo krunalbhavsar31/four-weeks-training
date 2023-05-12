7ee7142fce7bbdb4135e846e89520036928d8dfa
krunalbhavsar31 <krunal93bhavsar@gmail.com>
2023-05-12 16:58:00 +0530

    FileLoggerDisposableApp

--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -5,26 +5,67 @@
         static void Main(string[] args)
         {
             // Use FileLogger and dispose of it properly
+            using (var logger = new FileLogger("KB.txt"))
+            {
+                //logger.Log("Starting log");
+                //logger.Log("This is a log entry");
+                //logger.Log("Another log entry");
+                //logger.Log("Ending log");
+                logger.Log("I am Krunal...!");
+                logger.Log("I want to be a Software Developer.....!!");
+                logger.Dispose();
+            }
+            Console.WriteLine("Log entries written to file.");
         }
     }
 
     class FileLogger : IDisposable
     {
-        private StreamWriter _writer;
+        private StreamWriter _streamWriter;
+        private bool _disposed = false;
 
         public FileLogger(string filePath)
         {
             // Initialize StreamWriter instance
+            _streamWriter = new StreamWriter(filePath, append: true);
         }
 
         public void Dispose()
         {
             // Implement IDisposable pattern
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         public void Log(string message)
         {
             // Write message to the file
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(nameof(FileLogger));
+            }
+            _streamWriter.WriteLine(DateTime.Now.ToString() + " " + message);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    if (_streamWriter != null)
+                    {
+                        _streamWriter.Dispose();
+                        _streamWriter = null;
+                    }
+                }
+                _disposed = true;
+            }
+        }
+
+        ~FileLogger()
+        {
+            Dispose(false);
         }
     }
 }
